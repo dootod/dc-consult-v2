a4e975ab365ab4a24a5fcb40ae35f5134acdb36c
dootod <181655113+dootod@users.noreply.github.com>
2026-02-07 11:07:06 +0100

    Ajout des controlleurs publics du site

--- src/Controller/ClientsController.php
+++ src/Controller/ClientsController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class ClientsController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/clients', name: 'app_clients')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('clients/index.html.twig', [
+            'controller_name' => 'ClientsController',
         ]);
     }
 }
--- src/Controller/ProjetsController.php
+++ src/Controller/ProjetsController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class ProjetsController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/projets', name: 'app_projets')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('projets/index.html.twig', [
+            'controller_name' => 'ProjetsController',
         ]);
     }
 }
