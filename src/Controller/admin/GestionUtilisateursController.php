774baa0c9841d239b3d2974bd1f0449fbec73415
dootod <181655113+dootod@users.noreply.github.com>
2026-02-08 18:54:13 +0100

    Fonction ajouter utilisateur terminé

--- src/Controller/admin/GestionUtilisateursController.php
+++ src/Controller/admin/GestionUtilisateursController.php
@@ -4,10 +4,13 @@ namespace App\Controller\admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\UtilisateurRepository;
 use App\Entity\Utilisateur;
 use App\Form\NewUtilisateurType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class GestionUtilisateursController extends AbstractController
 {
@@ -28,13 +31,48 @@ final class GestionUtilisateursController extends AbstractController
     }
 
     #[Route('/admin/gestion-utilisateurs/nouveau', name: 'app_new_utilisateurs')]
-    public function new(): Response
+    public function new(
+        Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $passwordHasher): Response
     {
+        $utilisateur = new Utilisateur();
 
-        $utilisateur = $this->createForm(NewUtilisateurType::class);
+        $form = $this->createForm(NewUtilisateurType::class, $utilisateur);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $plainPassword = $form->get('password')->getData();
+            
+            $hashedPassword = $passwordHasher->hashPassword(
+                $utilisateur,
+                $plainPassword
+            );
+            $utilisateur->setPassword($hashedPassword);
+            
+            $roleChoice = $form->get('role_choice')->getData();
+            
+            if ($roleChoice === 'ROLE_ADMIN') {
+                $utilisateur->setRoles(['ROLE_ADMIN']);
+            } else {
+                $utilisateur->setRoles([]);
+            }
+            
+            $manager->persist($utilisateur);
+            $manager->flush();
+            
+            $this->addFlash(
+                'success',
+                'Utilisateur ajouté avec succès !'
+            );
+
+            return $this->redirectToRoute('app_show_utilisateurs', [
+                'id' => $utilisateur->getId(),
+            ]);
+        }
 
         return $this->render('admin/gestion_utilisateurs/new.html.twig', [
-            'form' => $utilisateur,
+            'form' => $form,
         ]);
     }
 }
--- src/Form/NewUtilisateurType.php
+++ src/Form/NewUtilisateurType.php
@@ -8,18 +8,39 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class NewUtilisateurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
-            ->add('nom')
-            ->add('prenom')
-            ->add('ajouter', SubmitType::class)
+            ->add('nom', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe'
+            ])
+            ->add('role_choice', ChoiceType::class, [
+                'label' => 'Type d\'utilisateur',
+                'mapped' => false,
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
+                ],
+                'placeholder' => 'Choisir un rôle',
+            ])
+            ->add('ajouter', SubmitType::class, [
+                'label' => 'Ajouter'
+            ])
         ;
     }
 
