ca2aa6556445f0d0e33f2009b7dfe16c9ca61a1b
dootod <181655113+dootod@users.noreply.github.com>
2026-02-08 17:44:00 +0100

    Changement mineur

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -5,12 +5,19 @@ namespace App\Controller\admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UtilisateurRepository;
 
 final class AdminController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'app_dashboard_admin')]
-    public function dashboard(): Response
+    public function dashboard(UtilisateurRepository $utilisateurRepository): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        $email = $this->getUser()?->getUserIdentifier();
+
+        $utilisateur = $utilisateurRepository->findOneBy(['email' => $email]);
+
+        return $this->render('admin/dashboard.html.twig', [
+            'prenom' => $utilisateur?->getPrenom(),
+        ]);
     }
 }
